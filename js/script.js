095c414de7ec97093befdc08abe1454f69579f8f
Adam Basiak <basiakus@gmail.com>
2017-11-18 08:34:01 +0100

    pushing exercise

---
+++ js/script.js
@@ -0,0 +1,22 @@
+function Button(text) {
+   this.text = text || "empty new button\(\)";
+   console.log(this.text);
+}
+Button.prototype.create = function() {
+   var self = this;
+   this.$element = $('<button>');
+   this.$element.text(this.text);
+         this.$element.click(function() {
+         alert(self.text);
+      });
+   $('body').append(this.$element);   
+   
+}
+var btn1 = new Button();
+$('#jq_creater').click(function() {
+   btn1.create();
+});
+var adamButton = new Button("hej Adam!");
+$('#jq_adam').click(function() {
+ adamButton.create();
+});
