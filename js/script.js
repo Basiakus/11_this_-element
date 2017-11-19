e2f76f39971d886d76c1a1089bcab1edf8acd054
Adam Basiak <basiakus@gmail.com>
2017-11-19 18:19:57 +0100

    beautify files

--- js/script.js
+++ js/script.js
@@ -1,25 +1,25 @@
 function Button(text) {
-   this.text = text || "empty new button\(\)";
-   console.log(this.text);
+    this.text = text || "empty new button\(\)";
+    console.log(this.text);
 }
 Button.prototype.create = function() {
-   var self = this;
-   this.$element = $('<button class="del">');
-   this.$element.text(this.text);
-         this.$element.click(function() {
-         alert(self.text);
-      });
-   $('body').append(this.$element);   
-   
+    var self = this;
+    this.$element = $('<button class="del">');
+    this.$element.text(this.text);
+    this.$element.click(function() {
+        alert(self.text);
+    });
+    $('body').append(this.$element);
+
 }
 var btn1 = new Button();
 $('#jq_creater').click(function() {
-   btn1.create();
+    btn1.create();
 });
 var adamButton = new Button("hej Adam!");
 $('#jq_adam').click(function() {
- adamButton.create();
+    adamButton.create();
 });
-$('#remove').click(function(){
-   $(".del").last().remove();
+$('#remove').click(function() {
+    $(".del").last().remove();
 });
